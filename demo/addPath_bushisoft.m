0f4becd88ae7840d506bc5a5c1a0a95d9d220374
Cao Tri DO <caotri.do88@gmail.com>
2016-12-03 22:40:55 +0100

    demo\addPath_bushisoft.m added

---
+++ demo/addPath_bushisoft.m
@@ -0,0 +1,48 @@
+function [ pathstr ] = addPath_bushisoft( )
+%ADDPATH_M2TML Summary of this function goes here
+%   Detailed explanation goes here
+
+% [pathstr,name,ext] = fileparts(pwd); 
+[pathstr,tmpname] = fileparts(pwd);
+if strcmp(tmpname,'demo')==0
+    error('Please launch this program in the demo folder !!')
+end
+if max(ismember(strsplit(path,';'), [pathstr,'/subFunctions'])) == 0
+    addpath(genpath([pathstr,'/subFunctions']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/data'])) == 0
+    addpath(genpath([pathstr,'/data']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/classes'])) == 0
+    addpath(genpath([pathstr,'/classes']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/externalLibs'])) == 0
+    addpath(genpath([pathstr,'/externalLibs']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/main'])) == 0
+    addpath(genpath([pathstr,'/main']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/demo'])) == 0
+    addpath(genpath([pathstr,'/demo']));
+end
+
+
+% if max(ismember(strsplit(path,';'), [pwd,'\externalToolbox'])) == 0
+%     addpath(genpath([pwd,'\externalToolbox']));
+% end
+
+if max(ismember(strsplit(path,';'), [pathstr,'/externalLibs'])) == 0
+    addpath(genpath([pathstr,'/externalLibs']));
+end
+
+if max(ismember(strsplit(path,';'), [pathstr,'\results'])) == 0
+    addpath(genpath([pathstr,'/results']));
+end
+
+end
+
