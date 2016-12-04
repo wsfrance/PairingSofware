127ea67ceecf814c519738c46dadac639669bcc1
Cao Tri DO <caotri.do88@gmail.com>
2016-12-04 22:05:29 +0100

    * added Capital_FirstLetter.m : 1st Capital letters in the Table for all strings

---
+++ subFunctions/Capital_FirstLetter.m
@@ -0,0 +1,36 @@
+function [ out_table ] = Capital_FirstLetter( array_str, column )
+%CAPITAL_FIRSTLETTER Summary of this function goes here
+%   Detailed explanation goes here
+
+out_table = array_str;
+
+for k = 1:size(column,2)
+    column_k = column{k};
+    subtable = array_str{:,{column_k}};
+    out_array_str = capitalize(subtable);
+    out_table(:,column_k) = out_array_str;
+end
+
+
+
+
+end
+
+
+function out_array_str = capitalize(array_str)
+
+[m,n]=size(array_str);
+out_array_str = cell(m,n);
+
+for i = 1:m
+    for j = 1:n
+        str = array_str{i,j};
+        % str='this is a tEST';
+        str=lower(str);
+        idx=regexp([' ' str],'(?<=\s+)\S','start')-1;
+        str(idx)=upper(str(idx));
+        out_array_str{i,j} = str;
+    end
+end
+
+end
