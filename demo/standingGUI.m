3535096c8a8ae441813a569b5d6bf52aefe9957f
Cao Tri DO <caotri.do88@gmail.com>
2016-12-06 11:53:41 +0100

    * modify : ** demo\standingGUI.fig ** demo\standingGUI.m
    * functions :
    ** looking for how to auto-size column in UITables
    ** Add print menu

--- demo/standingGUI.m
+++ demo/standingGUI.m
@@ -22,7 +22,7 @@ function varargout = standingGUI(varargin)
 
 % Edit the above text to modify the response to help standingGUI
 
-% Last Modified by GUIDE v2.5 05-Dec-2016 21:10:08
+% Last Modified by GUIDE v2.5 06-Dec-2016 11:16:54
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -78,6 +78,12 @@ columnTable = subtable.Properties.VariableNames;
 data = table2cell(subtable(:,columnTable));
 set(handles.TAB_standing, 'data', data, 'ColumnName', columnTable)
 
+% Maybe to put in code
+% source : http://undocumentedmatlab.com/blog/uitable-cell-colors/
+% Re-size columns width
+% autoResizeTable( handles.TAB_standing )
+
+
 % --- Outputs from this function are returned to the command line.
 function varargout = standingGUI_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
@@ -95,3 +101,13 @@ function BUT_columnSelection_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 column2displayGUI
+
+
+% --------------------------------------------------------------------
+function MENU_print_Callback(hObject, eventdata, handles)
+% hObject    handle to MENU_print (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+printpreview
+
+
