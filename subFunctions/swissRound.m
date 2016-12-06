70d41163f98ff6d2cc425b1df212d686c56d481c
Cao Tri DO <caotri.do88@gmail.com>
2016-12-06 17:06:38 +0100

    * correct ** subFunctions\swissRound.m ** problem in converting the IDplayer into WSCode

--- subFunctions/swissRound.m
+++ subFunctions/swissRound.m
@@ -160,7 +160,8 @@ function pairingWSCode = id2WSCode(tablePlayers, pairingID)
 [m,n] = size(pairingID);
 for i = 1:m
     for j = 1:n
-        pairingWSCode(i,j) = tablePlayers.WSCode(pairingID(i,j));
+        id = find(tablePlayers.playerId == pairingID(i,j));
+        pairingWSCode(i,j) = tablePlayers.WSCode(id);
     end
 end
 
