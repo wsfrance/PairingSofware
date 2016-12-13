ff17221b77c99626437a1be0b626ecc081ba1e65
Cao Tri DO <caotri.do88@gmail.com>
2016-12-13 10:34:49 +0100

    * modify ** case insensivitive option in the search of strings

--- subFunctions/GUI/strfind_idx.m
+++ subFunctions/GUI/strfind_idx.m
@@ -1,9 +1,19 @@
-function [ row, col, v ] = strfind_idx( Table, str )
+function [ row, col, v ] = strfind_idx( Table, str, caseInsensitiveOption )
 %STRFIND_IDX Summary of this function goes here
 %   Detailed explanation goes here
 
 % Table = tablePlayers_fromDB.WSCode;
 % str   = WSCode_i;
+
+if nargin < 3
+    caseInsensitiveOption = true;
+    disp('No caseInsensitiveOption. Putting by default to true')
+end
+
+if caseInsensitiveOption
+    Table = lower(Table);
+    str = lower(str);
+end
 
 IndexC= strfind(Table, str);
 [row, col, v] = find(not(cellfun('isempty', IndexC)));
