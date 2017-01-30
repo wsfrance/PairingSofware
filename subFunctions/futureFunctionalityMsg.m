a204e8418e34e254b592e85fe0182d2451d6921e
Cao Tri DO <caotri.do88@gmail.com>
2017-01-30 20:37:24 +0100

    add subFunctions\futureFunctionalityMsg.m

---
+++ subFunctions/futureFunctionalityMsg.m
@@ -0,0 +1,11 @@
+function [ output_args ] = futureFunctionalityMsg( )
+%FUTUREFUNCTIONALITYMSG Summary of this function goes here
+%   Detailed explanation goes here
+
+disp('--------------------------------------------------------------------')
+msg  = 'Function will be added in the future' ;
+disp(msg)
+msgbox(msg, 'Error', 'error')
+
+end
+
