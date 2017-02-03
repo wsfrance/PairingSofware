4fd3fea7365b549c66b3dcc862a800dcccf59b7f
Cao Tri DO <caotri.do88@gmail.com>
2017-02-03 14:30:56 +0100

    version 1.8

--- src/subFunctions/exportTable2html.m
+++ src/subFunctions/exportTable2html.m
@@ -1,4 +1,4 @@
-function [ output_args ] = exportTable2html( tableIn, filename, column, option);
+function [ output_args ] = exportTable2html( tableIn, filename, column, option)
 %EXPORTTABLE2HTML Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,7 +9,7 @@ colheads = tableIn.Properties.VariableNames;
 
 
 table_cell = [[colheads; data]]; %Add row and col heading cell-arrays onto a double data matrix
-caption_str = 'Test of HTML Table Output';
+caption_str = [option.tournamentInfo.name ' - Round no.' num2str(option.no_round)];
 
 
 html_table(table_cell, filename, 'Caption',caption_str, ...
