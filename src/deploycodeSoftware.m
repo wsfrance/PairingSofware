2df601e9e400fb8d8399bb7392797622f01af99f
Cao Tri DO <caotri.do88@gmail.com>
2017-02-06 15:52:50 +0100

    deploycodeSoftware.m

---
+++ src/deploycodeSoftware.m
@@ -0,0 +1,10 @@
+source_dir = 'E:\Cloud\Google Drive\WS\WS Sud France\2_Projet\H_Recode Pairing Software\code';
+target_dir = 'E:\Cloud\Google Drive\WS\WS Sud France\2_Projet\H_Recode Pairing Software\PublicRelease_tournamentSoftware';
+
+
+extension = {'\.txt$', '\.log$', '\.png$', '\.exe$', '\.html$', '\.ico$', '\.csv', '\.fig$', '\.zip$', '\.pdf$', ...
+             '\.class$', '\.java$', '\.jpg$', '\.c$', '\.p$'};
+
+ignoreList = {'^\.git$'};       
+         
+deploypcode(source_dir,target_dir,'updateOnly',true, 'flattenFileTree', false, 'copyDirectStrings', extension, 'ignoreStrings', ignoreList)
